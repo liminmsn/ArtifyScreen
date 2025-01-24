2cff0120de0d4ead57c02ed25f8b835ff8d2353e
杨再涛 <1772081518@qq.com>
2025-01-24 18:33:18 +0800

    添加跨域

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import reactLogo from "@/assets/react.svg";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import { Request } from "@/script/net/Request";
 
 
 function Home() {
@@ -13,40 +13,29 @@ function Home() {
         setGreetMsg(await invoke("greet", { name }));
     }
 
+    useEffect(() => {
+        new Request().GET("https://www.pexels.com/zh-cn/").then((res) => {
+            console.log(res);
+        });
+    }, []);
+
 
     return <>
-        <main className="container">
-            <h1>Welcome to Tauri + React</h1>
-
-            <div className="row">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-                </a>
-                <a href="https://tauri.app" target="_blank">
-                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-            <form
-                className="row"
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    greet();
-                }}
-            >
-                <input
-                    id="greet-input"
-                    onChange={(e) => setName(e.currentTarget.value)}
-                    placeholder="Enter a name..."
-                />
-                <button type="submit">Greet</button>
-            </form>
-            <p>{greetMsg}</p>
-        </main>
+        <form
+            className="row"
+            onSubmit={(e) => {
+                e.preventDefault();
+                greet();
+            }}
+        >
+            <input
+                id="greet-input"
+                onChange={(e) => setName(e.currentTarget.value)}
+                placeholder="Enter a name..."
+            />
+            <button type="submit">Greet</button>
+        </form>
+        <p>{greetMsg}</p>
     </>
 }
 
